a1ecdbe85b5ee2fd64ca0973d09605e833568687
lequocanh111 <lequocanh14012005@gmail.com>
2026-03-06 09:18:14 +0700

    V3: Stateless

--- Code Demo/app.py
+++ Code Demo/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request
 
-app = Flask(__name__, static_folder='static', template_folder='static')
-books = [{"id": 1, "title": "Python UET"}] 
+app = Flask(__name__, static_folder='static')
+books = [{"id": 1, "title": "Python UET", "author": "UET"}]
 
 @app.route('/')
 def home(): return app.send_static_file('index.html')
 
 @app.route('/api/books', methods=['GET', 'POST'])
 def handle_books():
+    token = request.headers.get('Authorization')
+    if token != 'UET-SECRET-KEY':
+        return jsonify({"error": "Unauthorized - Phải có Token mới cho xem!"}), 401
+
     if request.method == 'GET':
-        return jsonify(books) 
+        return jsonify(books)
     
     if request.method == 'POST':
-        new_book = request.json 
+        new_book = request.json
         books.append(new_book)
-        return jsonify(new_book), 201 
+        return jsonify(new_book), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
